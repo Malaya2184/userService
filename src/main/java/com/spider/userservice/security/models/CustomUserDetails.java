845c1f022b8c70066c014c7ef5e6f816b8826901
Malaya2184 <malayakumarswain8@gmail.com>
2024-05-08 09:35:25 +0530

    created basic class for customuser details and user details service from existing spring core classes

---
+++ src/main/java/com/spider/userservice/security/models/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.spider.userservice.security.models;
+
+import com.spider.userservice.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/spider/userservice/security/services/CustomeUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.spider.userservice.security.services;
+
+import com.spider.userservice.models.User;
+import com.spider.userservice.repositories.UserRepository;
+import com.spider.userservice.security.models.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+
+//creatd this class so that it can create a custom user details service for us
+public class CustomeUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomeUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        Optional<User> response = userRepository.findByEmail(userEmail);
+        if (response.isEmpty()) {
+            throw new UsernameNotFoundException("user with user email/user name " + userEmail + " not found");
+        }
+        User user = response.get();
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+        return null;
+    }
+}
